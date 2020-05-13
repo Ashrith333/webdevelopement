b691c94a2007877caaf316b71d73a60dc7e965dd
ASHRITH REDDY Y <ashrith333@icloud.com>
2020-05-13 22:14:13 +0530

    edits

--- app.js
+++ app.js
@@ -2,19 +2,37 @@ var express = require("express");
 var bodyparser= require("body-parser");
 var app = express();
 var port = 3000;
-app.use(bodyparser());
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+var campground=[
+    {name:"sam1",  image:"https://homepages.cae.wisc.edu/~ece533/images/girl.png"}
+    ,{name:"sam2", image:"https://homepages.cae.wisc.edu/~ece533/images/girl.png"}
+    ,{name:"sam3", image:"https://homepages.cae.wisc.edu/~ece533/images/girl.png"}      
+];
+
+
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
-app.get("/ash",function(req,res){
-    var campground=[
-        {name:"sam1", image:"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}
-        ,{name:"sam2", image:"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}
-        ,{name:"sam3", image:"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}      
-]
-res.render("ash");
+app.get("/campgrounds",function(req,res){
+
+res.render("ash",{campground:campground});
 })
+
+app.post("/campgrounds",function(req,res){
+       var name = req.body.name;
+        var image =req.body.image;
+        var nuewcampground= {
+            name : name , image : image
+        };
+        campground.push(nuewcampground);
+        res.redirect("/campgrounds");
+});
+app.get("/campgrounds/new",function(req,res){
+    res.render("new")
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));

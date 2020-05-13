715f3fe8b09a585bb85c512db577fac8f3e86fef
ASHRITH REDDY Y <ashrith333@icloud.com>
2020-05-13 21:08:56 +0530

    edits

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require("express");
+var bodyparser= require("body-parser");
 var app = express();
 var port = 3000;
-app.set("view engine","ejs");
+app.use(bodyparser());
+
+app.set("view engine", "ejs");
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
+app.get("/ash",function(req,res){
+    var campground=[
+        {name:"sam1", image:"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}
+        ,{name:"sam2", image:"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}
+        ,{name:"sam3", image:"https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}      
+]
+res.render("ash");
+})
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
